d7b88d7c038378080bc00203e6c59203307f4372
Sunny <me@darkowlzz.space>
2016-12-13 10:05:21 +0530

    Add cobra cli support

---
+++ watcher/watcher.go
@@ -0,0 +1,48 @@
+package watcher
+
+import (
+  "log"
+  "os"
+  "os/exec"
+
+  "github.com/fsnotify/fsnotify"
+)
+
+func TerraformWatcher() {
+  watcher, err := fsnotify.NewWatcher()
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer watcher.Close()
+
+  done := make(chan bool)
+
+  go func() {
+    for {
+      select {
+      case event := <-watcher.Events:
+        if event.Op&fsnotify.Write == fsnotify.Write {
+          log.Println("###############################")
+          log.Println("modified file:", event.Name)
+          cmd := exec.Command("terraform", "validate")
+          cmd.Stdout = os.Stdout
+          cmd.Stderr = os.Stderr
+          log.Println("Validation...")
+          if err := cmd.Run(); err != nil {
+            log.Printf("Validation failed!\n\n")
+          } else {
+            log.Printf("No issues found.\n\n")
+          }
+        }
+      case err := <-watcher.Errors:
+        log.Println("error:", err)
+      }
+    }
+  }()
+
+  err = watcher.Add(".")
+  if err != nil {
+    log.Fatal(err)
+  }
+  <-done
+}
